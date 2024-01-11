6ee87659aa0d23d1dfc1d420e0de29d50467bce4
flightlessbird <102932289+alexflightlessbird@users.noreply.github.com>
2024-01-11 12:52:29 -0500

    Create progressBar.js

---
+++ apis/progressBar.js
@@ -0,0 +1,75 @@
+const { createCanvas } = require("canvas");
+
+function isValidHexColor(color) {
+  // Regular expression to match a valid hex color (3 or 6 characters, optionally starting with #)
+  const hexColorRegex = /^#?([0-9A-Fa-f]{3}){1,2}$/;
+  const isValid = hexColorRegex.test(color);
+  return isValid;
+}
+
+const progressBar = async (req, res) => {
+  const { bgcolor, maxval, val, fillcolor, barcolor } = req.query;
+
+  if (!maxval || !val) {
+    res.status(400).json({
+      error: "Required values must be defined - maxval & val",
+    });
+    return;
+  }
+
+  let fill;
+
+  if (fillcolor) {
+    if (!isValidHexColor(fillcolor)) {
+      res.status(400).json({ error: "Please provide valid hex codes" });
+      return;
+    } else {
+      fill = fillcolor;
+    }
+  } else {
+    fill = "005ce7";
+  }
+
+  let bar;
+
+  if (barcolor) {
+    if (!isValidHexColor(barcolor)) {
+      res.status(400).json({ error: "Please provide valid hex codes" });
+      return;
+    } else {
+      bar = barcolor;
+    }
+  } else {
+    bar = "c8c6d7";
+  }
+
+  const canvas = createCanvas(600, 100);
+  const context = canvas.getContext("2d");
+
+  if (bgcolor) {
+    if (!isValidHexColor(bgcolor)) {
+      res.status(400).json({ error: "Please provide valid hex codes" });
+      return;
+    } else {
+      context.fillStyle = `#${bgcolor}`;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+  } else {
+    context.filllStyle = "rgba(0, 0, 0, 0)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  context.fillStyle = `#${bar}`;
+  context.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+
+  const progressPercentage = (val / maxval) * 100;
+  const progressPercentFill = (progressPercentage / 100) * (canvas.width - 20);
+  context.fillStyle = `#${fill}`;
+  context.fillRect(10, 10, progressPercentFill, canvas.height - 20);
+
+  res.status(200);
+  res.setHeader("Content-Type", "image/png");
+  canvas.createPNGStream().pipe(res);
+};
+
+module.exports = progressBar;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 
 async function Init() {
   const app = express();
@@ -8,8 +9,10 @@ async function Init() {
   app.use(express.urlencoded({ extended: true }));
 
   const matchRegex = require("./apis/regex.js");
+  const progressBar = require("./apis/progressBar.js");
 
   app.post("/regex", matchRegex);
+  app.get("/progress-bar", progressBar);
 
   app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
