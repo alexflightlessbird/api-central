f84b09c3dfb7f4893eb54ffaeaa15f8f2dc2ba7f
flightlessbird <102932289+alexflightlessbird@users.noreply.github.com>
2024-02-12 06:49:46 -0500

    Add support for number sorting in arraySort.js

--- apis/strings/arraySort.js
+++ apis/strings/arraySort.js
@@ -1,3 +1,5 @@
+const isNumber = require("../../helper-functions/numbers/isNumber");
+
 const arraySort = async (req, res) => {
   try {
     const { inputArray } = req.body;
@@ -8,8 +10,23 @@ const arraySort = async (req, res) => {
         .json({ error: "inputArray must be an array of values" });
     }
 
-    const sortedArray = inputArray.slice().sort();
+    const numbersArray = [];
+    const textArray = [];
+
+    inputArray.forEach((value) => {
+      if (isNumber(value)) {
+        numbersArray.push(Number(value));
+      } else {
+        textArray.push(value);
+      }
+    });
+
+    const sortedNumbers = numbersArray.slice().sort((a, b) => a - b);
+    const sortedText = textArray.slice().sort();
+
+    const sortedArray = sortedNumbers.concat(sortedText);
     const sortedString = sortedArray.join(", ");
+
     res.status(200).json({ array: sortedArray, string: sortedString });
   } catch (err) {
     console.error("Error:", err);
