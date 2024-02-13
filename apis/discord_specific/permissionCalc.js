32f3f663204737f30dbe411280087436c4ebf4b6
flightlessbird <102932289+alexflightlessbird@users.noreply.github.com>
2024-02-13 02:41:01 -0500

    Add emoji support

--- apis/discord_specific/permissionCalc.js
+++ apis/discord_specific/permissionCalc.js
@@ -12,17 +12,25 @@ const permissionCalc = async (req, res) => {
         .json({ error: "Invalid value. Must be a non-negative number." });
     }
 
+    const emoji = ": " + decodeURIComponent(req.query.emoji) || "";
+
     const value = BigInt(req.query.value);
 
     const result = permissionCalculations(value);
 
     const truePerms = Object.keys(result).filter((perm) => result[perm]);
+    const emojiPerms = truePerms.map((perm) => `${perm}${emoji}`);
 
     if (req.query.type === "show_all") {
       return res.status(200).json(result);
     } else if (req.query.type === "string") {
       const truePermsString = truePerms.join(", ");
       return res.status(200).json({ permissions: truePermsString });
+    } else if (req.query.type === "emojistring") {
+      const emojiPermsString = emojiPerms.join(", ");
+      return res.status(200).json({ permissions: emojiPermsString });
+    } else if (req.query.type === "emoji") {
+      return res.status(200).json(emojiPerms);
     } else {
       return res.status(200).json(truePerms);
     }
